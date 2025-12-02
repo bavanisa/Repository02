64e79f69663377289c825dc216b3ef961d1d0fb6
bavanisa <banisaggu881@gmail.com>
2025-12-02 21:42:19 +0530

    initial commit

---
+++ MyMavenProject/src/test/java/com/example/MyMavenProject/fibonacciseries.java
@@ -0,0 +1,23 @@
+package com.example.MyMavenProject;
+import java.util.Scanner;
+
+public class fibonacciseries{
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the fibonacci series number : " );
+		int n=sc.nextInt();
+		int a=0 ,b= 1;		
+		for (int i=1;i<=n;i++) {
+			
+			System.out.print(a + " ");
+			int c= a+b;
+			a=b;
+			b=c;		
+		}
+		sc.close();
+		
+
+	}
+
+}
